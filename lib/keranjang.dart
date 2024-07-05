d217685d19a797a6b0cf68911bd0a0bc19e5ccfe
doraotari <doraotari@gmail.com>
2024-07-05 11:59:41 +0000

    pertemuan 5 juli part 2

--- lib/keranjang.dart
+++ lib/keranjang.dart
@@ -14,36 +14,74 @@ class KeranjangPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Consumer<ProviderProduk>(
-            builder: (context, value, child) =>
-               ListView.builder(
-                itemCount: value.jmlKeranjang,
-                itemBuilder: (context, index) {
-                  final produk = value.listProduk[index];
-                  return Dismissible(
-                    direction: DismissDirection.endToStart,
-                    onDismissed: (_) {
-                      Provider.of<ProviderProduk>(context,listen: false).hapusKeranjang(produk);
-                    },
-                    key: ValueKey(produk.id),
-                    background: Container(
-                      alignment: Alignment.centerRight,
-                      color: Colors.red,
-                      child: const Icon(Icons.delete,color: Colors.white,size: 32,),
-                    ),
-                    child: ListTile(
-                      leading: Image.network(produk.gambar, width: 45,),
-                      title: Text(produk.judul),
-                      subtitle: Text(formatRupiah(produk.harga)),
-                    ),
-                  );
-                }),
+          Expanded(
+            child: Consumer<ProviderProduk>(
+              builder: (context, value, child) =>
+                 ListView.builder(
+                  itemCount: value.jmlKeranjang,
+                  itemBuilder: (context, index) {
+                    final produk = value.listProduk[index];
+                    return Dismissible(
+                      direction: DismissDirection.endToStart,
+                      onDismissed: (_) {
+                        Provider.of<ProviderProduk>(context,listen: false).hapusKeranjang(produk);
+                      },
+                      key: ValueKey(produk.id),
+                      background: Container(
+                        alignment: Alignment.centerRight,
+                        color: Colors.red,
+                        child: const Icon(Icons.delete,color: Colors.white,size: 32,),
+                      ),
+                      child: ListTile(
+                        leading: Image.network(produk.gambar, width: 45,),
+                        title: Text(produk.judul),
+                        subtitle: Text(formatRupiah(produk.harga)),
+                      ),
+                    );
+                  }),
+              ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            height: 100,
+            child:const Column(
+              children: [
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                  Text('Total Qty'),
+                  Text('0'),
+                ],),
+                Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                  Text('Total Bayar'),
+                  Text('0'),
+                ],),
+              ],
             ),
-            Container(
-              child: Column(children: [
-                //tugas buat 2 buah tombol elevated
-                // 1 tulisan Cash on Delivery
-                //2  tulisan Transfer
+          ),
+            SizedBox(
+              height: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                ElevatedButton.icon(
+                  onPressed: () {}, 
+                  style:const ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.purple),
+                    foregroundColor: WidgetStatePropertyAll(Colors.white)
+                    ),
+                  
+                  label: Text('Cash on Delivery'),
+                  icon: Icon(Icons.pin_drop),
+                  ),
+                ElevatedButton.icon(
+                  onPressed:(){}, 
+                  label: Text('Transfer'),
+                  icon: Icon(Icons.qr_code),
+                  ),
               ],),
             )
         ],
